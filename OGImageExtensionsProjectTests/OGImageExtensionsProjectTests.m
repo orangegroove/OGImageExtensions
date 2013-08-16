4a69196460e1e1fc7308c4555f94ce41369782b9
Jesper Skrufve <jesper.skrufve@gmail.com>
2013-08-16 14:56:23 +0300

    Initial Commit

---
+++ OGImageExtensionsProjectTests/OGImageExtensionsProjectTests.m
@@ -0,0 +1,34 @@
+//
+//  OGImageExtensionsProjectTests.m
+//  OGImageExtensionsProjectTests
+//
+//  Created by Jesper on 8/16/13.
+//  Copyright (c) 2013 Orange Groove. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OGImageExtensionsProjectTests : XCTestCase
+
+@end
+
+@implementation OGImageExtensionsProjectTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
